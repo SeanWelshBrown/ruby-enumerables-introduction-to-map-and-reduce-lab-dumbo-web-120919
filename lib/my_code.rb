a5e92576ca880e314df115a2d06a90c545c48b67
Sean Welsh Brown <seanwelshbrown@gmail.com>
2019-12-04 02:47:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# My Code here....
+def map_to_negativize(source_array)
+  negative_array = []
+  i = 0 
+  while i < source_array.length do
+    negative_index = source_array[i] * -1
+    negative_array << negative_index
+  end
+  negative_array
+end
